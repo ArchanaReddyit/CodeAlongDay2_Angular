e3711c616ada94e97375b01803ce882acf1e2e81
ArchanaReddyit <archanareddy.it@gmail.com>
2020-09-02 11:06:38 +0400

    Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { DatabindingComponent } from './databinding/databinding.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, DatabindingComponent, AboutusComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
